008eefd66fdc2d384af32111117c63397d1d2378
Kerk <kerk@Kerks-MacBook-Pro.local>
2016-04-12 09:23:45 +0700

    add MenuTabBarController

---
+++ jibjib/MenuTabBarController.m
@@ -0,0 +1,37 @@
+//
+//  MenuTabBarController.m
+//  jibjib
+//
+//  Created by Kerk on 4/12/16.
+//  Copyright © 2016 Chulalongkorn University. All rights reserved.
+//
+
+#import "MenuTabBarController.h"
+
+@interface MenuTabBarController ()
+
+@end
+
+@implementation MenuTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
