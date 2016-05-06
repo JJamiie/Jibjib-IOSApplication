e3c243fb38d506e2e17e369bc8e8b84c9439d6d6
Kerk <kerk@Kerks-MacBook-Pro.local>
2016-05-06 15:07:08 +0700

    Reset everything

---
+++ Jibjib/Jibjib/FeedTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FeedTableViewController.swift
+//  Jibjib
+//
+//  Created by Kerk on 5/6/16.
+//  Copyright © 2016 Chulalongkorn University. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewController: UITableViewController {
+
+}
