8bc75529ad521278a6bd9beb71f8036b0992dff3
JJamie Rashata <jjamie_08@hotmail.com>
2016-05-11 00:38:30 +0700

    set constraint new feed

--- Jibjib/Jibjib/FeedTableViewController.swift
+++ Jibjib/Jibjib/FeedTableViewController.swift
@@ -16,4 +16,17 @@ class FeedTableViewController: UITableViewController {
         super.viewDidLoad()
         self.header.frame =  CGRectMake(0 , 0, self.view.frame.width, 60)
     }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell_question",forIndexPath: indexPath)
+        return cell
+    }
 }
