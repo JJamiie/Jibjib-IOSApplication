82c9f130aadbf1983b84e88584b784571a3fc4cf
JJamie Rashata <jjamie_08@hotmail.com>
2016-05-10 22:47:19 +0700

    set constraint login

--- Jibjib/Jibjib/FeedTableViewController.swift
+++ Jibjib/Jibjib/FeedTableViewController.swift
@@ -10,4 +10,10 @@ import UIKit
 
 class FeedTableViewController: UITableViewController {
 
+    @IBOutlet weak var header: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.header.frame =  CGRectMake(0 , 0, self.view.frame.width, 60)
+    }
 }
--- Jibjib/Jibjib/LoginViewController.swift
+++ Jibjib/Jibjib/LoginViewController.swift
@@ -8,6 +8,38 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet weak var btn_sign_in: UIButton!
+    @IBOutlet weak var edt_username: UITextField!
+    @IBOutlet weak var edt_password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.edt_password.delegate = self
+        self.edt_username.delegate = self
+        setWidget()
+    }
+    
+    func setWidget(){
+        btn_sign_in.layer.cornerRadius = 6
+        borderButtom(edt_username)
+        borderButtom(edt_password)
+        edt_username.attributedPlaceholder = NSAttributedString(string:"username",attributes:[NSForegroundColorAttributeName: UIColor.whiteColor()])
+        edt_password.attributedPlaceholder = NSAttributedString(string:"password",attributes:[NSForegroundColorAttributeName: UIColor.whiteColor()])
+    }
+    
+    func borderButtom(textField:UITextField){
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        border.borderColor = UIColor.whiteColor().CGColor
+        border.frame = CGRect(x: 0, y: textField.frame.size.height - width, width:  textField.frame.size.width, height: textField.frame.size.height)
+        border.borderWidth = width
+        textField.layer.addSublayer(border)
+        textField.layer.masksToBounds = true
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
